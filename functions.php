326494e27b5e583c00dc8ec878a0d5aca84938f7
Fabiolx26 <fabio.lx26@gmail.com>
2023-08-13 17:02:03 +0200

    fix: master: minor fix

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
         wp_enqueue_style('techpros-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', array(), '4.5.2', 'all');
         wp_enqueue_style('material-icons', 'https://cdn.materialdesignicons.com/5.9.55/css/materialdesignicons.min.css', array(), '5.9.55', 'all');
         wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css', array(), '6.4.2', 'all');
-        wp_enqueue_style('animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css', array(), '4.1.1', 'all')
+        wp_enqueue_style('animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css', array(), '4.1.1', 'all');
         wp_enqueue_style('techpros-style', get_template_directory_uri() . '/style.css', array('techpros-bootstrap', 'material-icons', 'font-awesome', 'animate-css'), $version, 'all');
     }
     
