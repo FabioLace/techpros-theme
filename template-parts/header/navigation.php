aeb97723bc71c35937fb05ee8b1e47c6c5c0ae99
Fabio Lacerra <fabio.lx26@gmail.com>
2023-08-14 12:38:36 +0200

    feat: master: created navbar with styles and fake links

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -1,49 +1,78 @@
 <nav class="d-flex flex-row">
     <img src="wp-content/themes/techpros/assets/images/main-logo.png">
     <div class="nav-links d-flex flex-row">
-        <div class="nav-link">
-            <a href="#home">Home</a>
-            <div class="menu-tendina">
-
+        <div id="Home" class="nav-link">
+            <a href="#">Home</a>
+            <div class="drop-down"><!--JS-->
+                <a href="#">HOME ONE</a>
+                <a href="#">HOME TWO</a>
+                <a href="#">HOME THREE</a>
+                <a href="#">HOME FOUR</a>
+                <a href="#">HOME FIVE</a>
+                <a href="#">HOME SIX</a>
+                <a href="#">FAQ</a>
+            </div>
+        </div>
+        <div id="Company" class="nav-link">
+            <a href="#">Company</a>
+            <div class="drop-down">
+                <a href="#">About Us Two</a>
+                <a href="#">Why Choose Us</a>
+                <a href="#">Team Member</a>
+                <a href="#">Single Team</a>
+                <a id="portfolio" href="#">Portfolio</a>
+                    <div id="portfolio" class="drop-down sub-menu">
+                        <a href="#">Portfolio Two</a>
+                        <a href="#">Portfolio Three</a>
+                    </div>
+                <a id="our-service" href="#">Our Service</a>
+                    <div id="our-service" class="drop-down sub-menu">
+                        <a href="#">Our Service Two</a>
+                        <a href="#">Our Service Three</a>
+                    </div>
+                <a href="#">Case study</a>
+                <a href="#">Pricing plan</a>
+                <a href="#">Faq</a>
+            </div>
+        </div>
+        <div id="It-solution" class="nav-link">
+            <a href="#">IT Solution</a>
+            <div class="drop-down">
+                <a href="#">IT Services</a>
+                <a href="#">Managed IT Services</a>
+                <a href="#">Industries</a>
+                <a href="#">Business Solutions</a>
+                <a href="#">IT Services Details</a>
             </div>
         </div>
         <div class="nav-link">
-            <a href="#home">Home</a>
-            <div class="menu-tendina">
-
+            <a href="#">Elements</a>
+            <div class="drop-down">
+                <a href="#">Services</a>
+                <a href="#">Info Box</a>
+                <a href="#">Pricing Plan</a>
+                <a href="#">Team</a>
+                <a href="#">Countdown</a>
+                <a href="#">Accordion</a>
             </div>
         </div>
         <div class="nav-link">
-            <a href="#home">Home</a>
-            <div class="menu-tendina">
-
+            <a href="#">Blog</a>
+            <div class="drop-down">
+                <a href="#">Blog List</a>
+                <a href="#">Blog Grid</a>
+                <a href="#">Blog 2column</a>
             </div>
         </div>
         <div class="nav-link">
-            <a href="#home">Home</a>
-            <div class="menu-tendina">
-
+            <a href="#">Contact</a>
+            <div class="drop-down">
+                <a href="#">Contact Style One</a>
+                <a href="#">Contact Style Two</a>
+                <a href="#">Contact Style Three</a>
+                <a href="#">Contact Style Four</a>
             </div>
         </div>
-        <div class="nav-link">
-            <a href="#home">Home</a>
-            <div class="menu-tendina">
-
-            </div>
-        </div>
-        <div class="nav-link">
-            <a href="#home">Home</a>
-            <div class="menu-tendina">
-
-            </div>
-        </div>
-        <div class="nav-link">
-            <a href="#home">Home</a>
-            <div class="menu-tendina">
-
-            </div>
-        </div>
-
-        <button>Bottone</button>
+        <button>Get a quote</button>
     </div>
 </nav>
