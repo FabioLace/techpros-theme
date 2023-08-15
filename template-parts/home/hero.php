d66f8208506591497315930bd59c3b63a3e0a06d
Fabio Lacerra <fabio.lx26@gmail.com>
2023-08-15 17:20:41 +0200

    feat: master: created draft hero section [to fix background image and fonts]

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -1,4 +1,16 @@
 <div class="hero">
-    <img src="wp-content/themes/techpros/assets/images/slider1.jpg">
-
+    <div class="hero-content">
+        <div class="text">
+            <h4> // Full Cycle IT Solution Services </h4>
+            <h1 class="font-600">End-To-End<br>Secure IT Service </h1>
+            <p>
+                Over the years, a wide range of developments and innovations in the
+                global IT arena have led to many new IT-enabled
+            </p>
+        </div>
+        <div class="hero-buttons">
+            <button class="button how-it-works">How it works</button>
+            <button class="button it-service">It service</button>
+        </div>
+    </div>
 </div>
