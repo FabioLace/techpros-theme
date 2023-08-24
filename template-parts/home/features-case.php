138d75cb1ea3d269016a89f9608f2d301c3fff45
Fabio Lacerra <fabio.lx26@gmail.com>
2023-08-24 15:18:42 +0200

    feat: master: loaded new images, granted permissions to all users [GNU/Linux and Unix-like]

--- template-parts/home/features-case.php
+++ template-parts/home/features-case.php
@@ -9,16 +9,33 @@
             TechPros partners with businesses across every major industry to make amazing products and connect the dots between people.
         </p>
     </div>
-    <!-- <div class="slider">
-        <div class="case">
-            <img src="#" />
-            <div class="mini-card">
-                <div class="mini-card-content">
-                    <h3>Case Title</h3>
-                    <p>Design / Marketing</p>
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="case">
+                    <img src="/wp-content/uploads/2023/08/case5.jpg" /><!-- <img src="/wp-content/themese/techpros/assets/images/case5.jpg" /> -->
+                    <div class="mini-card">
+                        <div class="mini-card-content">
+                            <h3>Case Title</h3>
+                            <p>Design / Marketing</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+            </div>
+            <div class="swiper-slide">
+            </div>
+            <div class="swiper-slide">
+            </div>
+            <div class="swiper-slide">
+            </div>
+            <div class="swiper-slide">
+            </div>
+            <div class="swiper-slide">
+            </div>
+    </div>
+    <!-- <div class="slider">
+        
         <div class="case">
             <img src="#" />
             <div class="mini-card">
