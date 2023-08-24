218193fe646d20886b4eb99caa31a30843b438ee
Fabio Lacerra <fabio.lx26@gmail.com>
2023-08-24 17:38:40 +0200

    feat: master: created process.php base page, commented swiper script and slides in features.php

--- template-parts/home/features-case.php
+++ template-parts/home/features-case.php
@@ -9,11 +9,27 @@
             TechPros partners with businesses across every major industry to make amazing products and connect the dots between people.
         </p>
     </div>
-    <div class="swiper">
+    <script>
+        /* document.addEventListener('DOMContentLoaded', function () {
+            var swiper = new Swiper('.swiper', {
+                slidesPerView: 1,
+                spaceBetween: 0,
+                breakpoints: {
+                    1024: {
+                        slidesPerView: 2,
+                    },
+                    768: {
+                        slidesPerView: 1,
+                    },
+                },
+            });
+        }); */
+    </script>
+<!-- <div class="swiper">
         <div class="swiper-wrapper">
             <div class="swiper-slide">
                 <div class="case">
-                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case5.jpg" /><!-- <img src="/wp-content/themese/techpros/assets/images/case5.jpg" /> -->
+                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case5.jpg" />
                     <div class="mini-card">
                         <div class="mini-card-content">
                             <h3>Case Title</h3>
@@ -23,7 +39,7 @@
                 </div>
             </div>
             <div class="swiper-slide">
-                <!-- <div class="case">
+                <div class="case">
                     <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case4.jpg" />
                     <div class="mini-card">
                         <div class="mini-card-content">
@@ -31,52 +47,41 @@
                             <p>Design / Marketing</p>
                         </div>
                     </div>
-                </div> -->
+                </div>
             </div>
             <div class="swiper-slide">
-                <!-- <div class="case">
-                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case4.jpg" />
+                <div class="case">
+                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case3.jpg" />
                     <div class="mini-card">
                         <div class="mini-card-content">
                             <h3>Case Title</h3>
                             <p>Design / Marketing</p>
                         </div>
                     </div>
-                </div> -->
+                </div>
             </div>
             <div class="swiper-slide">
-                <!-- <div class="case">
-                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case4.jpg" />
+                <div class="case">
+                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case2.jpg" />
                     <div class="mini-card">
                         <div class="mini-card-content">
                             <h3>Case Title</h3>
                             <p>Design / Marketing</p>
                         </div>
                     </div>
-                </div> -->
+                </div>
             </div>
             <div class="swiper-slide">
-                <!-- <div class="case">
-                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case4.jpg" />
+                <div class="case">
+                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case1.jpg" />
                     <div class="mini-card">
                         <div class="mini-card-content">
                             <h3>Case Title</h3>
                             <p>Design / Marketing</p>
                         </div>
                     </div>
-                </div> -->
-            </div>
-            <div class="swiper-slide">
-                <!-- <div class="case">
-                    <img src="http://localhost/demobonny/wp-content/uploads/2023/08/case4.jpg" />
-                    <div class="mini-card">
-                        <div class="mini-card-content">
-                            <h3>Case Title</h3>
-                            <p>Design / Marketing</p>
-                        </div>
-                    </div>
-                </div> -->
+                </div>
             </div>
         </div>
-    </div>
+    </div>-->
 </div>
--- template-parts/home/process.php
+++ template-parts/home/process.php
@@ -1,2 +1,64 @@
-<div class="process">
+<div class="process d-flex flex-column align-items-center text-center">
+    <div class="text">
+        <div class="h5-line features d-flex flex-column align-items-center">
+            <h5>Process</h5>
+            <div class="line features-line"></div>
+        </div>
+        <h2>Our Working Process</h2>
+        <div class="step-container d-flex flex-row">
+            <div class="step">
+                <span class="circle-icon-counter">
+                    <span class="circle"></span>
+                    <i class="fa-solid fa-network-wired"></i>
+                    <span class="counter">1</span>
+                </span>
+                <div class="dot-line">
+                    <div class="dot"></div>
+                    <div class="vertical-line"></div>
+                </div>
+                <div class="text">
+                    <h5>Select a problem</h5>
+                    <p>
+                        We have the technology and industry expertise to develop solutions that can connect people and businesses.
+                    </p>
+                </div>
+            </div>
+            <!-- ARROW RIGHT -->
+            <div class="step">
+                <span class="circle-icon-counter">
+                    <span class="circle"></span>
+                    <i class="fa-solid fa-network-wired"></i>
+                    <span class="counter">2</span>
+                </span>
+                <div class="dot-line">
+                    <div class="dot"></div>
+                    <div class="vertical-line"></div>
+                </div>
+                <div class="text">
+                    <h5>Collect data</h5>
+                    <p>
+                        We have the technology and industry expertise to develop solutions that can connect people and businesses.
+                    </p>
+                </div>
+            </div>
+            <!-- ARROW RIGHT -->
+            <div class="step">
+                <span class="circle-icon-counter">
+                    <span class="circle"></span>
+                    <i class="fa-solid fa-network-wired"></i>
+                    <span class="counter">3</span>
+                </span>
+                <div class="dot-line">
+                    <div class="dot"></div>
+                    <div class="vertical-line"></div>
+                </div>
+                <div class="text">
+                    <h5>Process Data</h5>
+                    <p>
+                        We have the technology and industry expertise to develop solutions that can connect people and businesses.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
