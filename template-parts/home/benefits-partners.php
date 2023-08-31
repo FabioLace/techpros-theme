1c2deadc88e138e8891e54771be77fe3492c43ae
fabio.lacerra <fabio.lx26@gmail.com>
2023-08-31 14:02:53 +0200

    feat: master: creating benefits-partners page

--- template-parts/home/benefits-partners.php
+++ template-parts/home/benefits-partners.php
@@ -1,6 +1,52 @@
 <div class="benefits-partners">
-    <div class="benefits">
-        <h1 class="text-danger">OUR BENEFITS</h1>
+    <div class="benefits d-flex flew-row justify-content-center align-items-center">
+        <img class="collage" src="wp-content/themes/techpros/assets/images/why-choose.png">
+        <div class="text">
+            <div class="h5-line d-flex flex-row align-items-center">
+                <h5>Our benefits</h5>
+                <div class="line"></div>
+            </div>
+            <h2>Few Reasons Why You<br> Should Choose Us</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
+            </p>
+            <div class="paragraphs">
+                <div class="paragraph">
+                    <span class="icon">
+                        <i class="fa-solid fa-layer-group"></i>
+                    </span>
+                    <div class="paragraph-text">
+                        <h2>Product Designs</h2>
+                        <p>
+                            High standards of professionalism, integrity. Establishment of close working relationships.
+                        </p>
+                    </div>
+                </div>
+                <div class="paragraph">
+                    <span class="icon">
+                        <i class="fa-regular fa-handshake"></i>
+                    </span>
+                    <div class="paragraph-text">
+                        <h2>Big Data Analysis</h2>
+                        <p>
+                            Statistical analysis to help an organization to gain insights from large information web sets.
+                        </p>
+                    </div>
+                </div>
+                <div class="paragraph">
+                    <span class="icon">
+                        <i class="fa-regular fa-handshake"></i>
+                    </span>
+                    <div class="paragraph-text">
+                        <h2>Mantain App Data</h2>
+                        <p>
+                            We Create the optimal platform to develop and run digital applications for our clients app.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="partners">
         <h1 class="text-danger">SLIDER PARTNERS</h1>
