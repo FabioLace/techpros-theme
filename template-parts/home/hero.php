0c82715a5239fcb4f7ae4a15db908bce71f1d052
FabioLace <fabio.lx26@gmail.com>
2024-02-06 13:12:06 +0100

    fix: refactoring: minor fixes to styles

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -1,55 +1,57 @@
-<div class="hero">
-    <div class="hero-content">
-        <div class="text">
-            <h4 class="animate__animated animate__fadeInUp"> // Full Cycle IT Solution Services </h4>
-            <h1 class="animate__animated animate__fadeInUp animate__delay-1s">End-To-End<br>Secure IT Service </h1>
-            <p class="animate__animated animate__fadeInUp animate__delay-1s">
-                Over the years, a wide range of developments and innovations in the
-                global IT arena have led to many new IT-enabled
-            </p>
+<section class="hero">
+    <div class="container">
+        <div class="hero-content">
+            <div class="text">
+                <div class="over-title animate__animated animate__fadeInUp"> // Full Cycle IT Solution Services </h4>
+                <h1 class="animate__animated animate__fadeInUp animate__delay-1s">End-To-End<br>Secure IT Service </h1>
+                <p class="animate__animated animate__fadeInUp animate__delay-1s">
+                    Over the years, a wide range of developments and innovations in the
+                    global IT arena have led to many new IT-enabled
+                </p>
+            </div>
+            <div class="hero-buttons animate__animated animate__fadeInUp animate__delay-1s">
+                <button class="button how-it-works">
+                    How it works <i class="fa-solid fa-angle-right"></i>
+                </button>
+                <button class="button it-service">
+                    It service <i class="fa-solid fa-angle-right"></i>
+                </button>
+            </div>
         </div>
-        <div class="hero-buttons animate__animated animate__fadeInUp animate__delay-1s">
-            <button class="button how-it-works">
-                How it works <i class="fa-solid fa-angle-right"></i>
-            </button>
-            <button class="button it-service">
-                It service <i class="fa-solid fa-angle-right"></i>
-            </button>
+        <div class="main-services">
+            <div class="service service-1 service-white">
+                <div class="service-content">
+                    <div class="icon-h2">
+                        <i class="fa-solid fa-wallet" id="purple-icon"></i>
+                        <div class="service-name">IT Consultancy</div>
+                    </div>
+                    <div class="service-text">
+                        There are many variations of passages of the Lorem Ipsum available, but this is an majority have suffered.
+                    </div>
+                </div>
+            </div>
+            <div class="service service-2 service-purple">
+                <div class="service-content">
+                    <div class="icon-h2">
+                        <i class="fa-solid fa-microchip"></i>
+                        <div class="service-name">Cyber Security</div>
+                    </div>
+                    <div class="service-text">
+                        There are many variations of passages of the Lorem Ipsum available, but this is an majority have suffered.
+                    </div>
+                </div>
+            </div>
+            <div class="service service-3 service-white">
+                <div class="service-content">
+                    <div class="icon-h2">
+                        <i class="fa-solid fa-briefcase-clock" id="purple-icon"></i>
+                        <div class="service-name">24/7 online support</div>
+                    </div>
+                    <div class="service-text">
+                        There are many variations of passages of the Lorem Ipsum available, but this is an majority have suffered.
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="main-services">
-        <div class="service service-1 service-white">
-            <div class="service-content">
-                <div class="icon-h2">
-                    <i class="fa-solid fa-wallet" id="purple-icon"></i>
-                    <h2>IT Consultancy</h2>
-                </div>
-                <div class="service-text">
-                    There are many variations of passages of the Lorem Ipsum available, but this is an majority have suffered.
-                </div>
-            </div>
-        </div>
-        <div class="service service-2 service-purple">
-            <div class="service-content">
-                <div class="icon-h2">
-                    <i class="fa-solid fa-microchip"></i>
-                    <h2>Cyber Security</h2>
-                </div>
-                <div class="service-text">
-                    There are many variations of passages of the Lorem Ipsum available, but this is an majority have suffered.
-                </div>
-            </div>
-        </div>
-        <div class="service service-3 service-white">
-            <div class="service-content">
-                <div class="icon-h2">
-                    <i class="fa-solid fa-briefcase-clock" id="purple-icon"></i>
-                    <h2>24/7 online support</h2>
-                </div>
-                <div class="service-text">
-                    There are many variations of passages of the Lorem Ipsum available, but this is an majority have suffered.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+</section>
